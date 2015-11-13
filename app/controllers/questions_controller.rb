bbe0cf10fe0ab00c4603c17c45b635b25b5844e0
Ildar Sharifullin <ildorf16rus@gmail.com>
2015-11-13 22:12:00 +0300

    Add User model

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :load_question, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -19,6 +20,7 @@ class QuestionsController < ApplicationController
   	@question = Question.new(question_params)
   	
   	if @question.save
+      flash[:notice] = 'Your question successfully created.'
   		redirect_to @question
   	else
   		render :new
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -30,7 +30,8 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #new' do
-  	before {get :new}
+  	sign_in_user
+    before {get :new}
 
   	it 'assigns a new Question to @question' do
   		expect(assigns(:question)).to be_a_new(Question)
@@ -42,6 +43,7 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #edit' do
+    sign_in_user
   	before {get :edit, id: question}
 
   	 it 'assigns a requested Question to @question' do
@@ -54,6 +56,8 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'POST #create' do
+    sign_in_user
+
   	context 'with vilid attributes' do
   		it 'save new question in the database' do
   			expect { post :create, question: attributes_for(:question) }.to change(Question, :count).by(1)
@@ -78,6 +82,8 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'PATCH #update' do
+    sign_in_user
+
   	context 'with vilid attributes' do
   		it 'assigns requested question to @question' do
 				patch :update, id: question, question: attributes_for(:question)
@@ -112,6 +118,8 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'DELETE #destroy' do
+    sign_in_user 
+    
   	before { question }
 		it 'deletes question' do
 			expect { delete :destroy, id: question }.to change(Question, :count).by(-1)
