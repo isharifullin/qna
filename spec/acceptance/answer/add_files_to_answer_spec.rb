b6097f15300fe7b368ffc8eab91c64a9e721dd6d
Ildar Sharifullin <ildorf16rus@gmail.com>
2015-11-30 00:21:36 +0300

    Multiole attach

--- spec/acceptance/answer/add_files_to_answer_spec.rb
+++ spec/acceptance/answer/add_files_to_answer_spec.rb
@@ -9,21 +9,77 @@ feature 'Add files to answer', %q{
   given(:user) { create(:user) }
   given(:question) { create(:question) }
 
-  background do
-    sign_in(user)
-    visit question_path(question)
+  background { sign_in(user) }
+
+  describe 'when create answer' do
+    before { visit question_path(question) }
+
+    scenario 'User adds file', js: true do
+      fill_in 'Your answer', with: 'MyText'
+
+      click_link 'Add file'
+      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+      
+      click_on 'Post your answer'
+
+      within '.answers' do
+        expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      end
+    end
+
+    scenario 'User adds several files', js: true do
+      fill_in 'Your answer', with: 'MyText'
+
+      click_link 'Add file'
+      click_link 'Add file'
+
+      all("input[type='file']").first.set("#{Rails.root}/spec/spec_helper.rb")
+      all("input[type='file']").last.set("#{Rails.root}/spec/acceptance_helper.rb")
+
+      click_on 'Post your answer'
+      
+      within '.answers' do
+        expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+        expect(page).to have_link 'acceptance_helper.rb', href: '/uploads/attachment/file/2/acceptance_helper.rb'
+      end
+    end
   end
 
-  scenario 'User add file when gives answer', js: true do
-    fill_in 'Your answer', with: 'MyText'
+  describe 'when edit answer' do
+    given!(:answer) { create(:answer, question: question, user: user) }
 
-    click_link 'Add file'
-    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
-    
-    click_on 'Post your answer'
+    before { visit question_path(question) }
 
-    within '.answers' do
-      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    scenario 'User adds file', js: true do
+      within "#answer_#{answer.id}" do
+        click_link 'Edit'
+        fill_in 'Your answer', with: 'MyText'
+
+        click_link 'Add file'
+        attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+        
+        click_on 'Save'
+
+        expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      end
+    end 
+
+    scenario 'User adds several files', js: true do
+      within "#answer_#{answer.id}" do
+        click_link 'Edit'
+        fill_in 'Your answer', with: 'MyText'
+
+        click_link 'Add file'
+        click_link 'Add file'
+
+        all("input[type='file']").first.set("#{Rails.root}/spec/spec_helper.rb")
+        all("input[type='file']").last.set("#{Rails.root}/spec/acceptance_helper.rb")
+
+        click_on 'Save'
+        
+        expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+        expect(page).to have_link 'acceptance_helper.rb', href: '/uploads/attachment/file/2/acceptance_helper.rb'
+      end
     end
-  end 
+  end
 end
--- spec/acceptance/question/add_files_to_question_spec.rb
+++ spec/acceptance/question/add_files_to_question_spec.rb
@@ -7,21 +7,79 @@ feature 'Add files to question', %q{
 } do
 
   given(:user) { create(:user) }
+  given(:question) { create(:question, user: user) }
+ 
+  background { sign_in(user) }
 
-  background do
-    sign_in(user)
-    visit new_question_path
+  describe 'when create question' do
+    before { visit new_question_path }
+
+    scenario 'User adds file', js: true do
+      fill_in 'Title', with: 'MyString'
+      fill_in 'Body', with: 'MyText'
+
+      click_link 'Add file'
+      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+
+      click_on 'Create'
+
+      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    end 
+
+    scenario 'User adds several files', js: true do
+      fill_in 'Title', with: 'MyString'
+      fill_in 'Body', with: 'MyText'
+
+      click_link 'Add file'
+      click_link 'Add file'
+
+      all("input[type='file']").first.set("#{Rails.root}/spec/spec_helper.rb")
+      all("input[type='file']").last.set("#{Rails.root}/spec/acceptance_helper.rb")
+
+      click_on 'Create'
+
+      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      expect(page).to have_link 'acceptance_helper.rb', href: '/uploads/attachment/file/2/acceptance_helper.rb'
+    end
   end
 
-  scenario 'User add file when asks question', js: true do
-    fill_in 'Title', with: 'MyString'
-    fill_in 'Body', with: 'MyText'
+  describe 'when edit question' do
+    before { visit question_path(question) }
 
-    click_link 'Add file'
-    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    scenario 'User adds file', js: true do   
+      within '.question' do
+        click_link 'Edit'
+        
+        fill_in 'Title', with: 'MyString'
+        fill_in 'Body', with: 'MyText'
 
-    click_on 'Create'
+        click_link 'Add file'
+        attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
 
-    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
-  end 
+        click_on 'Save'
+
+        expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      end
+    end 
+
+    scenario 'User adds several files', js: true do
+      within '.question' do
+        click_link 'Edit'
+
+        fill_in 'Title', with: 'MyString'
+        fill_in 'Body', with: 'MyText'
+
+        click_link 'Add file'
+        click_link 'Add file'
+
+        all("input[type='file']").first.set("#{Rails.root}/spec/spec_helper.rb")
+        all("input[type='file']").last.set("#{Rails.root}/spec/acceptance_helper.rb")
+
+        click_on 'Save'
+
+        expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+        expect(page).to have_link 'acceptance_helper.rb', href: '/uploads/attachment/file/2/acceptance_helper.rb'
+      end
+    end
+  end
 end
