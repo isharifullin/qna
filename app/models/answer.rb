b686d8a81ceabfc49346961a4265cff94aab8871
Ildar Sharifullin <ildorf16rus@gmail.com>
2015-11-29 11:15:02 +0300

    fix

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -9,7 +9,7 @@ class Answer < ActiveRecord::Base
   def make_best
     Answer.transaction do
       question.answers.update_all(best: false)
-      update(best: true)  
+      update!(best: true)  
     end
   end
 end
