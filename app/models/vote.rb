ea788e7af6d604a7025deb2af57d2b6bfee5dcc0
Ildar Sharifullin <ildorf16rus@gmail.com>
2016-02-13 22:09:21 +0300

    fix vote validation and rating

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,10 +2,14 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :votable, polymorphic: true
 
-  validates :user_id, :votable, :value, presence: true
+  validates :user_id, presence: true, uniqueness: { scope: [:votable_type, :votable_id] }
+  validates :votable, :value, presence: true
   validates :value, inclusion: { in: [-1, 1] }
 
   scope :upvotes, -> { where(value: 1) }
   scope :downvotes, -> { where(value: -1) }
-  scope :rating, -> { sum(:value)}
+
+  def self.rating
+    sum(:value)
+  end
 end
